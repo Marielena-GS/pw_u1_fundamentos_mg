835e37619a0e7facb419d289709198d35d97114c
pokedex <mgonzalez@uce.edu.ec>
2025-11-10 19:47:43 -0500

    Taller_8
    Calculadora Final

---
+++ js/calculadoraFinal.js
@@ -0,0 +1,35 @@
+let num1 = 0;
+let num2 = 0;
+
+function mostrarDisplay(valor){
+
+    let elemento = document.getElementById('display');
+
+    elemento.innerText = elemento.innerText + valor;
+}
+
+function sumar(num1, num2){
+
+}
+
+function restar(num1, num2){
+
+}
+
+function multiplicar(num1, num2){
+
+}
+
+function dividir(num1, num2){
+
+}
+
+function porcentaje(num1, num2){
+
+}
+
+function clearDisplay(){
+    let elemento = document.getElementById('display');
+    elemento.innerText = "";
+}
+
